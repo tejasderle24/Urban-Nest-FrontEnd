943fd7a797a5989a7a9cfcf25fa68cd2e32420c6
tejasderle24 <tejasderle24@gmail.com>
2025-03-27 18:00:23 +0530

    Authorization Login Signup ForgetPwd ResetPwd

---
+++ client/src/components/Auth/index.js
@@ -0,0 +1,7 @@
+import Login from './Login'
+import Signup from './Signup'
+import ForgotPassword from './ForgotPassword'
+import ResetPassword from './ResetPassword'
+
+export {Login, Signup, ForgotPassword, ResetPassword};
+
