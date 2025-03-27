a9caf5df3c5b9af676e11f1b57faedc74f990080
tejasderle24 <tejasderle24@gmail.com>
2025-03-27 17:57:09 +0530

    Creating Properties Components

---
+++ client/src/pages/Properties.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropertiesPage from '../components/properties/Propertiespage'
+
+const Properties = () => {
+  return (
+    <div>
+      <PropertiesPage />
+    </div>
+  )
+}
+
+export default Properties
+
+
+
+
+
