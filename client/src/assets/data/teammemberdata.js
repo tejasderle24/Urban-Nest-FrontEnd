ccb2e05ff6ae8b02ebafb0fa094c62337830b985
tejasderle24 <tejasderle24@gmail.com>
2025-03-27 18:02:19 +0530

    data

---
+++ client/src/assets/data/teammemberdata.js
@@ -0,0 +1,38 @@
+import kundan from '/kundan.png';
+import tejas from '/tejas.png';
+
+export const teamMembers = [
+    {
+      name: 'Kundan Patil',
+      position: 'Founder & CEO',
+      bio: 'With 15+ years in real estate, Nakul leads our vision of transforming property search.',
+      image: kundan,
+      social: {
+        linkedin: '#',
+        twitter: '#',
+        instagram: '#',
+      },
+    },
+    {
+      name: 'Tejas Derle',
+      position: 'Chief Technology Officer',
+      bio: "Tech innovator driving our platform's cutting-edge solutions.",
+      image: tejas,
+      social: {
+        linkedin: '#',
+        twitter: '#',
+        instagram: '#',
+      },
+    },
+    {
+      name: 'Omkar Somawanshi',
+      position: 'Chief Technology Officer',
+      bio: "Tech innovator driving our platform's cutting-edge solutions.",
+      image: kundan,
+      social: {
+        linkedin: '#',
+        twitter: '#',
+        instagram: '#',
+      },
+    }
+  ];
