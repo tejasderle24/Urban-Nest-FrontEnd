e1910288c8c8a938fc0a25fbbfdcdb46121b2a2c
tejasderle24 <tejasderle24@gmail.com>
2025-03-27 17:58:50 +0530

    Pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+// import Properties from "./Properties";
+import Contact from "./Contact";
+import Aboutus from "./Aboutus";
+import Properties from "./Properties";
+
+export { Home, Contact, Aboutus, Properties };
