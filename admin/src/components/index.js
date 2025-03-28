54cbca96d49a90268c62b140e46beb2334d9c511
tejasderle24 <tejasderle24@gmail.com>
2025-03-28 12:12:17 +0530

    Creating Components

---
+++ admin/src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import ProtectedRoute from "./ProtectedRoute";
+import ErrorFallback from "./ErrorFallback";
+import Login from "./Login";
+
+
+export { Navbar, ProtectedRoute, ErrorFallback, Login };
